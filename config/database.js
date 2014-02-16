a749687fda71f2a44e99f38a7f23382d5b69625e
Andrew <andrew.selvia@iCloud.com>
2014-02-16 17:22:29 -0600

    initial commit, integration of authentication + controller

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+	// 'url' : 'postgres://andrewselvia:password@localhost/postgres'
+	// 'url' : 'mongodb://andrewselvia:password@novus.modulusmongo.net:27017/Tod9ybax'
+	'url' : 'mongodb://localhost'
+};
