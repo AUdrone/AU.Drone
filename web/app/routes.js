0b0969d852edbce2d95955845420b934b1604444
Andrew <andrew.selvia@iCloud.com>
2014-02-24 00:49:58 -0600

    update model to use username, not email

--- web/app/routes.js
+++ web/app/routes.js
@@ -28,18 +28,18 @@ module.exports = function(app, passport) {
 	// SIGNUP ==============================
 	// =====================================
 	// show the signup form
-	// app.get('/signup', function(req, res) {
+	app.get('/signup', function(req, res) {
 
-	// 	// render the page and pass in any flash data if it exists
-	// 	res.render('signup.ejs', { message: req.flash('signupMessage') });
-	// });
+		// render the page and pass in any flash data if it exists
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
 
 	// process the signup form
-	// app.post('/signup', passport.authenticate('local-signup', {
-	// 	successRedirect : '/drone', // redirect to the secure profile section
-	// 	failureRedirect : '/signup', // redirect back to the signup page if there is an error
-	// 	failureFlash : true // allow flash messages
-	// }));
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/drone', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
 
 	// =====================================
 	// PROFILE SECTION =====================
