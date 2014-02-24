81fb7c33a708037dd21cd8e813372b1ea9fbcef3
Andrew <andrew.selvia@iCloud.com>
2014-02-23 23:42:49 -0600

    remove signup button from /index; prevent direct access to /drone and /signup; eliminate unnecessary model attributes; clean up /index and /login

--- web/app/routes.js
+++ web/app/routes.js
@@ -28,29 +28,29 @@ module.exports = function(app, passport) {
 	// SIGNUP ==============================
 	// =====================================
 	// show the signup form
-	app.get('/signup', function(req, res) {
+	// app.get('/signup', function(req, res) {
 
-		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
+	// 	// render the page and pass in any flash data if it exists
+	// 	res.render('signup.ejs', { message: req.flash('signupMessage') });
+	// });
 
 	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/drone', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-	}));
+	// app.post('/signup', passport.authenticate('local-signup', {
+	// 	successRedirect : '/drone', // redirect to the secure profile section
+	// 	failureRedirect : '/signup', // redirect back to the signup page if there is an error
+	// 	failureFlash : true // allow flash messages
+	// }));
 
 	// =====================================
 	// PROFILE SECTION =====================
 	// =====================================
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
+	// app.get('/profile', isLoggedIn, function(req, res) {
+	// 	res.render('profile.ejs', {
+	// 		user : req.user // get the user out of session and pass to template
+	// 	});
+	// });
 
 	// =====================================
 	// LOGOUT ==============================
@@ -60,9 +60,9 @@ module.exports = function(app, passport) {
 		res.redirect('/');
 	});
 
-	app.get('/drone', function(req, res) {
+	app.get('/drone', isLoggedIn, function(req, res) {
 		res.render('drone', {
-			user : req.user
+			// user : req.user
 		});
 	});
 };
